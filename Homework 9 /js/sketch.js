dccaa21304b531477abb67b9b78dd45dcef52625
madelinestaples <madeline.staples@umontana.edu>
2021-10-29 14:13:36 -0600

    Create sketch.js

---
+++ Homework 9 /js/sketch.js
@@ -0,0 +1,27 @@
+function setup()
+{
+    createCanvas(500, 600,);
+}
+function draw()
+{
+    background(220);
+    
+    point 
+    line (30,20,400, 75);
+    line (40, 80, 30 , 20);
+    fill('rgb(0,255,0)');
+    square (250, 210, 50);
+    rect (70, 40, 90, 60);
+    triangle(30, 75, 58, 20, 86, 75);
+    triangle (40, 70, 90, 20, 86, 75);
+    
+
+    fill('rgb(0,255,0)');
+    circle(70, 80, 50);
+    circle(170, 80, 50);
+   fill(51);
+   textSize(32);
+   text ('My Artwork', 10, 200);
+   text ('Madeline Staples',10,300);
+}
+
