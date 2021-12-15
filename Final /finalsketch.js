79b47697403d33fc7f1489d95f70155cef6af875
madelinestaples <madeline.staples@umontana.edu>
2021-12-15 15:26:40 -0700

    new

---
+++ Final /finalsketch.js
@@ -0,0 +1,82 @@
+function setup() {
+    createCanvas(800,900);
+  }
+  
+  function draw() {
+    
+   //pink background 
+   background('#fae');
+   // call createpinktriangle
+   createpinktriangle();
+   
+   // call createsquarerec
+   createsquarerec();
+   
+   // call createblktriangle
+   createblktriangle();
+
+   // call createblkcircle
+   createblkcircle();
+
+   //call createwhitecircle
+   createwhitecircle();
+
+   //call createpflower
+   createpflower();
+
+   //call createname
+   createname();
+  
+  
+
+   
+   
+    
+ 
+ 
+  
+  }
+  function createpinktriangle()
+  {
+    triangle(300, 100, 320, 100, 310, 80);
+  }
+  function createsquarerec()
+  {
+    square (250, 210, 50);
+    rect (70, 40, 90, 60);
+  }
+  function createblktriangle()
+    {
+      fill(0);
+   triangle(30, 75, 58, 20, 86, 75);
+    }
+  function createblkcircle()
+  {
+    fill(0);
+   circle(600,60,70);
+  }
+  function createwhitecircle()
+  {
+    fill(255);
+   circle(100,200,200);
+  }
+  function createpflower()
+  {
+    fill(204, 101, 192, 127);
+   stroke(127, 63, 120);
+    translate(580, 200);
+   noStroke();
+   for (let i = 0; i < 10; i ++) {
+    ellipse(0, 30, 20, 80);
+    rotate(PI/5);
+  }
+  }
+  function createname()
+  {
+    text ('Madeline Staples',10,300);
+  }
+  
+  
+  
+
+ 
